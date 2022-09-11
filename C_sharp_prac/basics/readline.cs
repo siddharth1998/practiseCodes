c1245d0e454e7b24250bf794a6647a28dd88c9e3
siddharth1998 <siddharth1998@gmail.com>
2022-09-11 23:53:43 +0530

    Adding the C# hello and readline

---
+++ C_sharp_prac/basics/readline.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+public class Program{
+	public static void Main(){
+	Console.WriteLine("Please Enter Your Name !");
+    string Name=Console.ReadLine();
+    Console.WriteLine("Your Name "+Name+".");
+	}
+}
